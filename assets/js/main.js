57d0bd36ef6e670f9119bd347c95451c67afb0c6
Giuditta <giudittamarino89@gmail.com>
2021-04-21 17:52:45 +0200

    inizio nav

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+      menuNavigation:
+      [
+        {
+          name: 'Home',
+          link: 'https://maxcoach.thememove.com/'
+        },
+
+        {
+          name: 'Pages',
+          link: 'https://maxcoach.thememove.com/main/courses/#'
+        },
+
+        {
+          name: 'Courses',
+          link: 'https://maxcoach.thememove.com/main/courses/'
+        },
+
+        {
+          name: 'Features',
+          link: 'https://maxcoach.thememove.com/main/events/'
+        },
+
+        {
+          name: 'Blog',
+          link: 'https://maxcoach.thememove.com/main/blog/'
+        },
+
+        {
+          name: 'Shop',
+          link: 'https://maxcoach.thememove.com/main/product/use-seo-to-boost-your-business/'
+        }
+      ]
+
+    },
+
+    mounted(){
+
+    },
+
+    methods: {
+
+    }
+
+
+});
