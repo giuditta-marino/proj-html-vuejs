b3bffc9f95befc6cc61a39a0d28e0b06dfab1f97
Giuditta <giudittamarino89@gmail.com>
2021-04-27 23:06:31 +0200

    aggiunta file dati

---
+++ src/js/dati.js
@@ -0,0 +1,304 @@
+menuNavigation=
+[
+  {
+    name: 'Home',
+    link: 'https://maxcoach.thememove.com/'
+  },
+
+  {
+    name: 'Pages',
+    link: 'https://maxcoach.thememove.com/main/courses/#'
+  },
+
+  {
+    name: 'Courses',
+    link: 'https://maxcoach.thememove.com/main/courses/'
+  },
+
+  {
+    name: 'Features',
+    link: 'https://maxcoach.thememove.com/main/events/'
+  },
+
+  {
+    name: 'Blog',
+    link: 'https://maxcoach.thememove.com/main/blog/'
+  },
+
+  {
+    name: 'Shop',
+    link: 'https://maxcoach.thememove.com/main/product/use-seo-to-boost-your-business/'
+  }
+];
+
+socials=
+[
+  {
+    name: 'Twitter',
+    iconName: 'twitter',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://twitter.com/'
+  },
+
+  {
+    name: 'Facebook',
+    iconName: 'facebook-f',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://www.facebook.com/'
+  },
+
+  {
+    name: 'Instagram',
+    iconName: 'instagram',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://www.instagram.com/'
+  },
+
+  {
+    name: 'Linkedin',
+    iconName: 'linkedin',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://it.linkedin.com/'
+  }
+];
+
+latestCourses=
+[
+  {
+    title: "How to be Successful: Create A Growth Mindset For Success",
+    price: '$30',
+    priceCents:'00',
+    previewFile: 'motivation-course-06-480x298.jpg',
+    lessonsNumber: '3',
+    studentsNumber: '50'
+  },
+
+  {
+    title: "How to Build Confidence in Your Abilities",
+    price: '$30',
+    priceCents:'00',
+    previewFile: 'motivation-course-05-480x298.jpg',
+    lessonsNumber: '1',
+    studentsNumber: '50'
+  },
+
+  {
+    title: "Productivity Machine - Focus in a Distracted World",
+    price: '$20',
+    priceCents:'00',
+    previewFile: 'motivation-course-04-480x298.jpg',
+    lessonsNumber: '5',
+    studentsNumber: '50'
+  },
+
+  {
+    title: "Effective Time Management Mastery - Complete Guide",
+    price: '$20',
+    priceCents:'00',
+    previewFile: 'motivation-course-03-480x298.jpg',
+    lessonsNumber: '18',
+    studentsNumber: '50'
+  },
+
+  {
+    title: "Body Language Secrets for Entrepeneurs",
+    price: '$25',
+    priceCents:'99',
+    previewFile: 'motivation-course-02-480x298.jpg',
+    lessonsNumber: '19',
+    studentsNumber: '50'
+  },
+
+  {
+    title: "Management Skills: The Science of Leadership",
+    price: '$19',
+    priceCents:'99',
+    previewFile: 'motivation-course-01-480x298.jpg',
+    lessonsNumber: '17',
+    studentsNumber: '50'
+  }
+];
+
+books=
+[
+  {
+    title: 'Alpha man',
+    author: 'Maxcoach',
+    price: '$29',
+    priceCents: '00',
+    previewFile: 'book-1.jpg'
+  },
+
+  {
+    title: 'Real man 4.0',
+    author: 'Maxcoach',
+    price: '$39',
+    priceCents: '00',
+    previewFile: 'book-2.jpg'
+  },
+
+];
+
+testimonials=
+[
+  {
+    name: 'Mina Hollace',
+    role: 'Freelancer',
+    reviewText: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+    imageFile: 'testimonial-avata-1.jpg',
+    number: '1'
+  },
+
+  {
+    name: 'Steven Malkovich',
+    role: 'Web developer',
+    reviewText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    imageFile: 'testimonial-avata-2.jpg',
+    number: '2'
+  },
+
+  {
+    name: 'Anthony Ballack',
+    role: 'Photographer',
+    reviewText: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    imageFile: 'testimonial-avata-3.jpg',
+    number: '3'
+  },
+
+  {
+    name: 'John Smith',
+    role: 'Teacher',
+    reviewText: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    imageFile: 'testimonial-avata-4.jpg',
+    number: '4'
+  }
+
+];
+
+articles=
+[
+  {
+    title: 'How to Stay True to Your Personal Brand',
+    textPreview: 'When it comes to your business or career, you want...',
+    date: 'May 13, 2020',
+    imageFile: 'motivation-blog-04-480x325.jpg'
+  },
+
+  {
+    title: '5 Vital Lessons in 5 Years of Freelancing',
+    textPreview: "Being self-employed and working from home, it's easy to get...",
+    date: 'May 13, 2020',
+    imageFile: 'motivation-blog-03-480x325.jpg'
+  },
+
+  {
+    title: '11 Super Useful Tips for Small-Business Owners',
+    textPreview: 'Being a small-business owner poses a ton of challenges. We...',
+    date: 'May 13, 2020',
+    imageFile: 'motivation-blog-02-480x325.jpg'
+  },
+
+  {
+    title: 'How to Give Yourself Grace to Start Again',
+    textPreview: 'Forgive yourself for bad habits you may started or...',
+    date: 'May 13, 2020',
+    imageFile: 'motivation-blog-01-480x325.jpg'
+  }
+
+];
+
+footerSocials=
+[
+
+  {
+    name: 'Facebook',
+    iconName: 'facebook-f',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://www.facebook.com/'
+  },
+
+  {
+    name: 'Twitter',
+    iconName: 'twitter',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://twitter.com/'
+  },
+
+  {
+    name: 'Instagram',
+    iconName: 'instagram',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://www.instagram.com/'
+  },
+
+  {
+    name: 'Linkedin',
+    iconName: 'linkedin',
+    iconPrefix: 'fa-',
+    iconFamily: 'fab',
+    link: 'https://it.linkedin.com/'
+  }
+];
+
+footerExplore=
+[
+  {
+    title: 'Start here',
+    link: '#'
+  },
+
+  {
+    title: 'Blog',
+    link: '#'
+  },
+
+  {
+    title: 'About us',
+    link: '#'
+  },
+
+  {
+    title: 'Success story',
+    link: '#'
+  },
+
+  {
+    title: 'Courses',
+    link: '#'
+  },
+
+  {
+    title: 'Contact us',
+    link: '#'
+  }
+];
+
+footerInformation=
+[
+  {
+    title: 'Membership',
+    link: '#'
+  },
+
+  {
+    title: 'Purchase guide',
+    link: '#'
+  },
+
+  {
+    title: 'Privacy policy',
+    link: '#'
+  },
+
+  {
+    title: 'Terms of service',
+    link: '#'
+  }
+];
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require ('laravel-mix');
 
-mix.js('src/js/main.js', 'assets/js/');
+mix.js('src/js/main.js', 'assets/js/').js('src/js/dati.js', 'assets/js');
 mix.sass('src/scss/master.scss', 'assets/css/').options({
     processCssUrls: false
 });
